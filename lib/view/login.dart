92c09b4261747156c9bdaeb70915656f3f3c5714
seanhii <seanhiijunwei@gmail.com>
2024-07-04 19:03:56 +0800

    remove unused import

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:sweat_kick/view/physicalMeetings.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:sweat_kick/view/register.dart';
 
